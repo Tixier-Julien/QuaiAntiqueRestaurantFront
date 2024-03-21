fc392f1a42b27162acedbe3aa64c2bdaa2c0dfdd
Tixier Julien <tixier.julien60@yahoo.fr>
2024-03-21 18:29:58 +0100

    add script.js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,7 @@ export const allRoutes = [
 
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", "/js/signin.js"),
     new Route("/signup", "Inscription", "/pages/auth/signup.html", "/js/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
