99a5b97bbb7817b1de4cd38b53d1e10b278bf5d0
Tixier Julien <tixier.julien60@yahoo.fr>
2024-03-22 13:59:34 +0100

    add button inscription nom prenom mail disabled

--- js/signup.js
+++ js/signup.js
@@ -6,6 +6,7 @@ const inputPrenom = document.getElementById("PrénomInput");
 const inputMail = document.getElementById("EmailInput");
 const inputPassword = document.getElementById("PasswordInput");
 const inputValidationPassword = document.getElementById("ValidatePasswordInput");
+const btnValidation = document.getElementById("btn-validation-inscription");
 
 inputNom.addEventListener("keyup", validateForm);
 inputPrenom.addEventListener("keyup", validateForm);
@@ -13,18 +14,45 @@ inputMail.addEventListener("keyup", validateForm);
 inputPassword.addEventListener("keyup", validateForm);
 inputValidationPassword.addEventListener("keyup", validateForm);
 
+//Fonction permettant de valider le formulaire
 function validateForm(){
-    validateRequired(inputNom);
-    validateRequired(inputPrenom);
+    const nomOk = validateRequired(inputNom);
+    const prenomOk =validateRequired(inputPrenom);
+    const mailOk = validateMail(inputMail);
+    
+    if(nomOk && prenomOk && mailOk){
+        btnValidation.disabled = false;
+    }
+    else {
+        btnValidation.disabled = true;
+    }
+}
+
+function validateMail(input){
+    //Définir mon regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const mailUser = input.value;
+    if(mailUser.match(emailRegex)){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true;
+    }
+    else {
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        return false;
+    }
 }
 
 function validateRequired(input){
     if(input.value != ''){
         input.classList.add("is-valid");
         input.classList.remove("is-invalid");
+        return true;
     }
     else {
         input.classList.remove("is-valid");
         input.classList.add("is-invalid");
+        return false;
     }
 }
