1b752f42b285dc225f67c8e06cf9e3ccafbe4681
Tixier Julien <tixier.julien60@yahoo.fr>
2024-03-22 11:19:50 +0100

    ajout validation nom prenom

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,8 @@
 export default class Route {
-    constructor(url, title, pathHtml, pathJs = ""){
+    constructor(url, title, pathHtml, pathJS = ""){
         this.url = url;
         this.title = title;
         this.pathHtml = pathHtml;
-        this.pathJs = pathJs;
+        this.pathJS = pathJS;
     }
 }
